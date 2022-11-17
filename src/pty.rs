66bf74c4483660a2782b2606c6b1e04d09ca7bae
Ege Balcı <egebalci@pm.me>
2022-11-17 16:39:24 +0300

    Code refactored.

--- src/pty.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(not(target_os = "windows"))]
-mod pty_nix;
-#[cfg(not(target_os = "windows"))]
-pub use pty_nix::*;
-#[cfg(target_os = "windows")]
-mod pty_win;
-#[cfg(target_os = "windows")]
-pub use pty_win::*;
